e3a980439f7a24e04a65cfa8dc8cffdde96b4e55
Chris Urwin <chrisu@hotmail.co.uk>
2017-05-17 12:59:37 +0100

    Updated NFS

--- scripts/install_nfs.sh
+++ scripts/install_nfs.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -ex
 
 echo Installing NFS server
-sudo mkdir /nfs
-sudo docker run -d --name nfs --privileged cpuguy83/nfs-server /nfs
+sudo ros service enable kernel-headers
+sudo ros service up kernel-headers
+sudo modprobe nfs
+sudo mkdir -p /home/rancher/nfs
+sudo docker run -d --name nfs --restart=always --privileged --net=host -v /home/rancher/nfs:/nfsshare -e SHARED_DIRECTORY=/nfsshare itsthenetwork/nfs-server-alpine:4
